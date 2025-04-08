d6ad408a9a1cd21e71c8128002a187c10a603d5b
Benjamin Team Rocket <benjamin@team.rocket.com>
2025-04-08 01:00:38 -0400

    feat: add Docker compose support for microservices

---
+++ microservices/gateway/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.4.2"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.benjamin.checkout"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+extra["springCloudVersion"] = "2024.0.1"
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+	implementation("org.springframework.boot:spring-boot-starter-aop")
+	implementation("io.micrometer:micrometer-registry-otlp")
+	implementation("io.micrometer:micrometer-tracing-bridge-brave")
+	implementation("io.zipkin.contrib.otel:encoder-brave:0.1.0")
+	runtimeOnly("com.github.loki4j:loki-logback-appender:1.5.2")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+  imports {
+    mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+  }
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
--- microservices/gateway/src/main/java/com/benjamin/checkout/gateway/config/GatewayConfig.java
+++ microservices/gateway/src/main/java/com/benjamin/checkout/gateway/config/GatewayConfig.java
@@ -1,5 +1,6 @@
 package com.benjamin.checkout.gateway.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -9,14 +10,20 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GatewayConfig {
 
+    @Value("${ecommerce.checkout.products.url}")
+    private String productServiceUrl;
+
+    @Value("${ecommerce.checkout.cart.url}")
+    private String cartServiceUrl;
+
     @Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("product-service", r -> r.path("/products/**")
-                        .uri("http://localhost:8081")
+                        .uri(productServiceUrl)
                 )
                 .route("cart-service", r -> r.path("/carts/**")
-                        .uri("http://localhost:8082")
+                        .uri(cartServiceUrl)
                 )
                 .build();
     }
