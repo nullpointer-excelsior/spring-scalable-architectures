7c1fba1624c39f659b2394fc78120778411d50a7
Benjamin Team Rocket <benjamin@team.rocket.com>
2025-04-08 20:50:34 -0400

    fix: update project name to 'cart-ms' and clean up dependencies

--- microservices/cart-ms/build.gradle.kts
+++ microservices/cart-ms/build.gradle.kts
@@ -25,9 +25,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	
 	implementation("org.springframework.cloud:spring-cloud-starter-config")
-
 	implementation("org.springframework.boot:spring-boot-starter-aop")
 	implementation("org.postgresql:postgresql:42.7.5")
 	implementation("io.micrometer:micrometer-registry-otlp")
