92c485ee1f715cee938c40fa5ec187efb9b2cd17
Benjamin Team Rocket <benjamin@team.rocket.com>
2025-04-10 16:37:55 -0400

    feat: add service discovery microservice

---
+++ microservices/service-discovery/src/test/java/com/benjamin/ecommerce/discovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benjamin.ecommerce.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
