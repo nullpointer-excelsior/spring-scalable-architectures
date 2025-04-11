3e55ae2356a6aa937fdc653198b330823481ee67
Benjamin Team Rocket <benjamin@team.rocket.com>
2025-04-11 00:15:23 -0400

    feat: add service discovery microservice to docker-compose and update config properties

--- microservices/service-discovery/build.gradle.kts
+++ microservices/service-discovery/build.gradle.kts
@@ -21,6 +21,7 @@ extra["springCloudVersion"] = "2024.0.1"
 
 dependencies {
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
