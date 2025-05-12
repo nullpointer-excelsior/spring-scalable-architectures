39e9e1c46f4b10aa76b0fb363a0990cd3ff8a613
Benjamin Team Rocket <benjamin@team.rocket.com>
2025-05-12 14:18:16 -0400

    feat(security): enhance user details service and update application configuration

--- secure-microservices/authserver/src/main/java/com/benjamin/ecommerce/authserver/config/SecurityConfig.java
+++ secure-microservices/authserver/src/main/java/com/benjamin/ecommerce/authserver/config/SecurityConfig.java
@@ -7,17 +7,18 @@ import org.springframework.http.MediaType;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
 
-import static org.springframework.aot.generate.ValueCodeGenerator.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -66,6 +67,24 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+   /* @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public MapReactiveUserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("customer")
+                .password(passwordEncoder().encode("customer"))
+                .roles("CUSTOMER")
+                .build();
+        UserDetails adminUser = User.withUsername("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+        return new MapReactiveUserDetailsService(user, adminUser);
+    }*/
 
     @Bean
     public UserDetailsService userDetailsService() {
